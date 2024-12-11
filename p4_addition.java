c79ffc710c187c46d758a43bbe983e81114cf9e7
rahul gurjar <rahulgurjar824582@gmail.com>
2024-12-11 14:29:34 +0530

    program for addition

---
+++ p4_addition.java
@@ -0,0 +1,12 @@
+public class p4_addition {
+    public static void main (String args []){
+        int num1 = 10;
+        int num2 = 20;
+        
+        System.err.println("addition :" + (num1+num2));
+        System.err.println("addition :" + (num1-num2));
+        System.err.println("addition :" + (num1*num2));
+        System.err.println("addition :" + (num1/num2));
+        System.err.println("addition :" + (num1%num2));
+    }
+}
